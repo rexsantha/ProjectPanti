734c6296ebe771c23b6c0406311b687531e4f85a
rexsantha <kingsantha16af@gmail.com>
2018-05-20 07:27:14 +0700

    Fungsi Sign IN dan Sign UP

--- app/src/main/java/com/example/andhikaeffendy/projectorang/AccountActivity/LoginActivity.java
+++ app/src/main/java/com/example/andhikaeffendy/projectorang/AccountActivity/LoginActivity.java
@@ -1,6 +1,9 @@
 package com.example.andhikaeffendy.projectorang.AccountActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,9 +24,24 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
 public class LoginActivity extends AppCompatActivity {
+    public static final String PREFS_NAME = "USER_DATA";
 
-
+    private String mUsername, mPassword;
+    private EditText mEmailET, mPassET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,12 +52,16 @@ public class LoginActivity extends AppCompatActivity {
 
         Button login = (Button) findViewById(R.id.btn_login);
         TextView signUp = (TextView) findViewById(R.id.signup_btn);
+        mEmailET = (EditText) findViewById(R.id.email_login);
+        mPassET = (EditText) findViewById(R.id.password_login);
 
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, BottomNavigationActivity.class);
-                startActivity(intent);
+                mUsername = mEmailET.getText().toString();
+                mPassword = mPassET.getText().toString();
+
+                new UserDataAsyncTask().execute();
             }
         });
 
@@ -50,5 +72,118 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    private void savePreferences(String username, String password){
+        SharedPreferences settings = getSharedPreferences(PREFS_NAME, 0);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putString("username", username).apply();
+        editor.putString("password", password).apply();
+
+        // Commit the edits!
+        editor.commit();
+    }
+
+    class UserDataAsyncTask extends AsyncTask<URL,Void,String> {
+        String BASE_URL="http://ioidev.com/login.php";
+        ProgressDialog dialog = new ProgressDialog(LoginActivity.this);
+
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("Sedang Mencoba Masuk.");
+            dialog.show();
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(URL... urls) {
+            String serverResponse = "";
+            URL url = null;
+            try{
+                url = createUrl(BASE_URL);
+            }catch(MalformedURLException e){
+                e.printStackTrace();
+            }
+            try{
+                serverResponse = makeHttpRequest(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return serverResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String strings) {
+            Log.d("webrespondd", strings);
+            if(dialog.isShowing()){
+                dialog.dismiss();
+            }
+            if(strings.toLowerCase().equals("sukses")){
+                savePreferences(mUsername,mPassword);
+                Intent tempIntent = new Intent(LoginActivity.this, BottomNavigationActivity.class);
+                tempIntent.putExtra("username", mUsername);
+                tempIntent.putExtra("password", mPassword);
+                startActivity(tempIntent);
+            }else{
+                Toast.makeText(getApplicationContext(),strings, Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        private String makeHttpRequest (URL add_url) throws IOException{
+            String jsonResponse = "";
+            try{
+                URL url = add_url;
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
+                String data = URLEncoder.encode("username", "UTF-8")
+                        + "=" + URLEncoder.encode(mUsername, "UTF-8");
+
+                data += "&" + URLEncoder.encode("password", "UTF-8") + "="
+                        + URLEncoder.encode(mPassword, "UTF-8");
+
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+                Log.d("resp", jsonResponse);
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return jsonResponse;
+
+
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonResponse;
+        }
+
+        private URL createUrl(String url) throws MalformedURLException {
+            return new URL(url);
+        }
+
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if(inputStream != null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while(line != null){
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return  output.toString();
+        }
+
     }
 }
--- app/src/main/java/com/example/andhikaeffendy/projectorang/AccountActivity/SignUpActivity.java
+++ app/src/main/java/com/example/andhikaeffendy/projectorang/AccountActivity/SignUpActivity.java
@@ -1,10 +1,12 @@
 package com.example.andhikaeffendy.projectorang.AccountActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,25 +19,140 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.nio.charset.Charset;
+
 public class SignUpActivity extends AppCompatActivity {
 
-
+    private String mFullName, mPassword, mEmail, mConfirmPassword;
+    private EditText mFullNameET, mPassET, mEmailET, mConfirmPassET;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
 
+        mFullNameET = findViewById(R.id.full_name);
+        mEmailET = findViewById(R.id.email);
+        mPassET = findViewById(R.id.password);
+        mConfirmPassET = findViewById(R.id.re_password);
+
         getSupportActionBar().hide();
 
         Button signUpBtn = (Button) findViewById(R.id.daftar_btn);
+
         signUpBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
-                startActivity(intent);
+                mFullName = mFullNameET.getText().toString();
+                mEmail = mEmailET.getText().toString();
+                mPassword = mPassET.getText().toString();
+
+                new UserRegisAsyncTask().execute();
             }
         });
         }
+
+    class UserRegisAsyncTask extends AsyncTask<URL,Void,String> {
+        String BASE_URL="http://ioidev.com/Registrasi.php";
+        @Override
+        protected String doInBackground(URL... urls) {
+            String serverResponse = "";
+            URL url = null;
+            try{
+                url = createUrl(BASE_URL);
+            }catch(MalformedURLException e){
+                e.printStackTrace();
+            }
+            try{
+                serverResponse = makeHttpRequest(url);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            return serverResponse;
+        }
+
+        @Override
+        protected void onPostExecute(String strings) {
+            Log.d("webrespondd", strings);
+            if(strings.toLowerCase().equals("sukses")){
+                Toast.makeText(SignUpActivity.this, "Daftar Berhasil", Toast.LENGTH_SHORT).show();
+                Intent tempIntent = new Intent(SignUpActivity.this, LoginActivity.class);
+                startActivity(tempIntent);
+            }else{
+                Toast.makeText(SignUpActivity.this, strings, Toast.LENGTH_SHORT).show();
+            }
+
+        }
+
+        private String makeHttpRequest (URL add_url) throws IOException{
+            String jsonResponse = "";
+            try{
+                URL url = add_url;
+                HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+
+                String data = URLEncoder.encode("nama", "UTF-8")
+                        + "=" + URLEncoder.encode(mFullName, "UTF-8");
+
+                data += "&" + URLEncoder.encode("password", "UTF-8") + "="
+                        + URLEncoder.encode(mPassword, "UTF-8");
+
+                data += "&" + URLEncoder.encode("email", "UTF-8") + "="
+                        + URLEncoder.encode(mEmail, "UTF-8");
+
+                bufferedWriter.write(data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+                Log.d("resp", jsonResponse);
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return jsonResponse;
+
+
+            }catch (MalformedURLException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return jsonResponse;
+        }
+
+        private URL createUrl(String url) throws MalformedURLException {
+            return new URL(url);
+        }
+
+        private String readFromStream(InputStream inputStream) throws IOException {
+            StringBuilder output = new StringBuilder();
+            if(inputStream != null){
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+                BufferedReader reader = new BufferedReader(inputStreamReader);
+                String line = reader.readLine();
+                while(line != null){
+                    output.append(line);
+                    line = reader.readLine();
+                }
+            }
+            return  output.toString();
+        }
+
+    }
 
 }
